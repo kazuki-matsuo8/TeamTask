849a3e14a79bc75955c39646c8322ca6ad8de14c
Kazuki Matsuo <kazuki161217@gmail.com>
2025-11-03 22:59:47 +0900

    #18 add&fix:JWT有効期限を12hに変更、ユーザー一覧APIを実装

--- teamtask-api/config/routes.rb
+++ teamtask-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create, :index]
       resources :teams, only: [:create] do
         # /api/v1/teams/:team_id/members
         resources :members, only: [:create]

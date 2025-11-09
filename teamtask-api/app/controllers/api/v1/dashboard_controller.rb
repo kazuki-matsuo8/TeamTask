dea9f5b142cc0ba1a24f52903ceab7470a296a99
Kazuki Matsuo <kazuki161217@gmail.com>
2025-11-09 21:49:19 +0900

    #38 fix:所属チームも取得できるように変更

--- teamtask-api/app/controllers/api/v1/dashboard_controller.rb
+++ teamtask-api/app/controllers/api/v1/dashboard_controller.rb
@@ -11,6 +11,9 @@ class Api::V1::DashboardController < ApplicationController
       }
     end
 
+    accepted_team_users = @current_user.team_users.accepted.includes(:team)
+    @my_teams = accepted_team_users.map(&:team)
+
     @upcoming_tasks = @current_user.tasks
                                 .where(status: [:todo, :inprogress])
                                 .where("deadline BETWEEN ? AND ?", Time.current, 3.days.from_now)
@@ -19,6 +22,7 @@ class Api::V1::DashboardController < ApplicationController
 
     render json: {
       pending_invitations: @pending_invitations,
+      my_teams: @my_teams,
       upcoming_tasks: @upcoming_tasks.as_json(include: [:team, :users])
     }
   end

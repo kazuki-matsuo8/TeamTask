364825b4749be2a0f1403c2798c36532a8981b80
Kazuki Matsuo <kazuki161217@gmail.com>
2025-11-30 21:17:43 +0900

    fix: CORS設定修正

--- teamtask-api/config/initializers/cors.rb
+++ teamtask-api/config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins  "http://127.0.0.1:5173"
     resource "*",
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options],

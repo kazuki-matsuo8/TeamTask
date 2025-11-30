fbb7ff26d3b9c97e9b5dcde6792f0a57fb0ce646
Kazuki Matsuo <kazuki161217@gmail.com>
2025-11-30 23:55:29 +0900

    fix: actioncableも修正

--- teamtask-api/config/initializers/action_cable.rb
+++ teamtask-api/config/initializers/action_cable.rb
@@ -6,6 +6,6 @@ Rails.application.config.action_cable.disable_request_forgery_protection = true
 
 # Allow connections from any origin in development
 Rails.application.config.action_cable.allowed_request_origins = [
-  'http://localhost:5173', 
+  'http://127.0.0.1:5173', 
   /http:\/\/localhost:*/   
 ]
